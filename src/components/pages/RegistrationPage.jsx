375019d0f9344a14ef551254dd029edec1ff901c
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-18 09:39:05 +0300

    test

--- src/components/pages/RegistrationPage.jsx
+++ src/components/pages/RegistrationPage.jsx
@@ -1,16 +1,12 @@
-import React, { useEffect } from 'react';
 import RegistrationForm from 'components/RegistrationForm/RegistrationForm';
 import Container from 'components/Container/Container';
 import { Helmet } from 'react-helmet';
 
 const RegistrationPage = () => {
-  useEffect(() => {
-    document.title = 'Sing in';
-  }, []);
   return (
     <Container>
       <Helmet>
-        <title>Registration</title>
+        <title>Log in</title>
       </Helmet>
       <RegistrationForm />
     </Container>
